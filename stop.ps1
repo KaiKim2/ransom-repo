2f1ea38f0df9532eba4c4b5fc6dbfbac393fbe08
Tech_kY <mr.annonymous2008@gmail.com>
2025-09-29 01:28:49 +0530

    Update stop.ps1

--- stop.ps1
+++ stop.ps1
@@ -1,29 +1,12 @@
-Add-Type @"
-using System;
-using System.Runtime.InteropServices;
+# Path to Sticky Keys registry key
+$regPath = "HKCU:\Control Panel\Accessibility\StickyKeys"
 
-public class KeyboardSimulator {
-    [DllImport("user32.dll")]
-    public static extern void keybd_event(byte bVk, byte bScan, int dwFlags, int dwExtraInfo);
+# Enable Sticky Keys by setting the "Flags" value
+# "510" = Sticky Keys enabled (standard)
+Set-ItemProperty -Path $regPath -Name "Flags" -Value "510"
 
-    public const int KEYEVENTF_KEYDOWN = 0x0000;
-    public const int KEYEVENTF_KEYUP   = 0x0002;
-    public const byte VK_SHIFT = 0x10;
-    public const byte VK_ENTER = 0x0D;
-}
-"@
+# Optional: Force settings to apply immediately
+Stop-Process -Name explorer -Force
+Start-Process explorer
 
-# Press Shift 5 times (key-down and key-up)
-for ($i = 0; $i -lt 5; $i++) {
-    [KeyboardSimulator]::keybd_event([KeyboardSimulator]::VK_SHIFT, 0, [KeyboardSimulator]::KEYEVENTF_KEYDOWN, 0)
-    Start-Sleep -Milliseconds 50
-    [KeyboardSimulator]::keybd_event([KeyboardSimulator]::VK_SHIFT, 0, [KeyboardSimulator]::KEYEVENTF_KEYUP, 0)
-    Start-Sleep -Milliseconds 50
-}
-
-# Wait 2 seconds for Sticky Keys dialog to appear
-Start-Sleep -Seconds 2
-
-# Press Enter to select "Yes"
-[KeyboardSimulator]::keybd_event([KeyboardSimulator]::VK_ENTER, 0, [KeyboardSimulator]::KEYEVENTF_KEYDOWN, 0)
-[KeyboardSimulator]::keybd_event([KeyboardSimulator]::VK_ENTER, 0, [KeyboardSimulator]::KEYEVENTF_KEYUP, 0)
+Write-Host "Sticky Keys enabled!"
